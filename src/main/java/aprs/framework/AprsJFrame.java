326d24079f8985b0d839bf04bbb3b53e669c1dbd
Will Shackleford <william.shackleford@nist.gov>
2016-06-23 16:42:36 -0400

    add license headers git ignores

--- src/main/java/aprs/framework/AprsJFrame.java
+++ src/main/java/aprs/framework/AprsJFrame.java
@@ -1,8 +1,26 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * This software is public domain software, however it is preferred
+ * that the following disclaimers be attached.
+ * Software Copywrite/Warranty Disclaimer
+ * 
+ * This software was developed at the National Institute of Standards and
+ * Technology by employees of the Federal Government in the course of their
+ * official duties. Pursuant to title 17 Section 105 of the United States
+ * Code this software is not subject to copyright protection and is in the
+ * public domain.
+ * 
+ * This software is experimental. NIST assumes no responsibility whatsoever 
+ * for its use by other parties, and makes no guarantees, expressed or 
+ * implied, about its quality, reliability, or any other characteristic. 
+ * We would appreciate acknowledgement if the software is used. 
+ * This software can be redistributed and/or modified freely provided 
+ * that any derivative works bear some notice that they are derived from it, 
+ * and any modified versions bear some notice that they have been modified.
+ * 
+ *  See http://www.copyright.gov/title17/92chap1.html#105
+ * 
  */
+ 
 package aprs.framework;
 
 import com.github.wshackle.visiontodb.Object2DViewJInternalFrame;
@@ -100,7 +118,7 @@ public class AprsJFrame extends javax.swing.JFrame implements ActionsToCrclDispl
         }
         try {
             setIconImage(ImageIO.read(AprsJFrame.class.getResource("aprs.png")));
-        } catch (IOException ex) {
+        } catch (Exception ex) {
             Logger.getLogger(AprsJFrame.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
